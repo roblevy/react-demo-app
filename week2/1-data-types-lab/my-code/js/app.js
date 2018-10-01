c606bc019c1b94660c1113ee39fa684ea78bbf65
Ellie Farrer <elliefarrermusic@gmail.com>
2018-10-01 10:21:33 +0100

    Added data types lab

---
+++ week2/1-data-types-lab/my-code/js/app.js
@@ -0,0 +1,109 @@
+// Using Google to help you, solve the following problems:
+//
+// > **Note:** When using Google, remember to add the language you are using. "How do I turn a string in to an array" is not as good as "How do I turn a string into an array with javascript"!
+//
+// 1. Turn a string into an array of words, using `String.split`:
+//     - Hi there, my name is Mike => ['Hi', 'there', 'my', 'name', 'is', 'Mike']
+
+const words = 'Hi there my name is Mike';
+const answer1 = null;
+console.log(answer1);
+
+// 2. Get `World` from the string `'Hello World!'` using `String.substr`:
+//     - `'Hello World!' => 'World'`
+
+const helloWord = 'Hello World!';
+const answer2 = null;
+console.log(answer2);
+
+// 3. Capitalize a word using a combination of `String.toUpperCase` and `String.substr`:
+// 	   - `roberto => 'Roberto'`
+
+const name = 'roberto';
+const answer3 = null;
+console.log(answer3);
+
+// 4. Convert a string to a number using `parseInt`
+// 	   - `'12' => 12`
+
+const integerAsString = '12';
+const answer4 = null;
+console.log(answer4);
+
+// 5. Convert a decimal number to a string with 2 decimal places using `Number.toFixed`
+// 	   - `15.827993 => '15.82'`
+
+const float = 15.827993;
+const answer5 = null;
+console.log(answer5);
+
+// 6. Join two arrays of strings using `Array.concat`.
+//     - ['Mike', 'Emily'] ['Will', 'Ajay'] => ['Mike', 'Emily', 'Will', 'Ajay']
+
+const array1 = ['Mike', 'Emily'];
+const array2 = ['Will', 'Ajay'];
+const answer6 = null;
+console.log(answer6);
+
+// 7. Take an array of words and turn them into a string with pipes `|` between each word using `Array.join`.
+//     - ['cloud', 'house', 'mountain', 'field'] => "cloud|house|mountain|field"
+
+const arrayOfThings = ['cloud', 'house', 'mountain', 'field'];
+const answer7 = null;
+console.log(answer7);
+
+// 8. Make a new array of the 2nd and 3rd elements of an array using `Array.slice`
+//     - ['hippo', 'giraffe', 'lion', 'hyena'] => ['giraffe', 'lion']
+
+const arrayOfAnimals = ['hippo', 'giraffe', 'lion', 'hyena'];
+const answer8 = null;
+console.log(answer8);
+
+// 9. Find the index of the word "pizza" in the following array with  `Array.indexOf`.
+//     - ['salad', 'burger', 'pizza', 'soup'] => 2
+
+const arrayOfFood = ['salad', 'burger', 'pizza', 'soup'];
+const answer9 = null;
+console.log(answer9);
+
+// 10. Sum the contents on an array of numbers using `Array.reduce`.
+//     - [1,2,3] => 6
+
+const arrayOfNumbers = [1,2,3];
+const answer10 = null;
+console.log(answer10);
+
+// 11. Take an array of strings and turn it into an array of numbers indicating the length of each string using `Array.map`.
+//     - ['Los Angeles', 'New York', 'Huston'] => [11, 8, 6]
+
+const arrayOfCities = ['Los Angeles', 'New York', 'Huston'];
+const answer11 = null;
+console.log(answer11);
+
+// 12. Take an array of numbers and return only the ones that are divisible by 3. You will need `Array.filter` and the modulus `%` operator.
+//     - [1,2,3,4,5,6,7,8,9,10] => [3,6,9]
+
+const largeArrayOfNumbers = [1,2,3,4,5,6,7,8,9,10];
+const answer12 = null;
+console.log(answer12);
+
+// 13. Order an array of strings alphabetically with `Array.sort`.
+//     - ['Mohammed', 'Katheryn', 'Aaron', 'Amanda'] => ['Aaron', 'Amanda', 'Katheryn', 'Mohammed']
+
+const arrayOfNames = ['Mohammed', 'Katheryn', 'Aaron', 'Amanda'];
+const answer13 = null;
+console.log(answer13);
+
+// 14. Turn an array of numbers into an array of strings using `Array.map` and `String(num)`
+//     - [5,10,15,20,25] => ['5','10','15','20','25']
+
+const mediumArrayOfNumbers = [5,10,15,20,25];
+const answer14 = null;
+console.log(answer14);
+
+// 13. Convert a phrase to title case, using `String.split`, `Array.map`, `String.toUpperCase`, `String.substr` and `Array.join`
+// 	    - `'the wind in the willows' => 'The Wind In The Willows'`
+
+const title = 'the wind in the willows';
+const answer15 = null;
+console.log(answer15);
---
+++ week2/1-data-types-lab/my-code/readme.md
@@ -0,0 +1,50 @@
+## Independent practise (40 mins)
+
+Using Google to help you, solve the following problems:
+
+> **Note:** When using Google, remember to add the language you are using. "How do I turn a string in to an array" is not as good as "How do I turn a string into an array with javascript"!
+
+1. Turn a string into an array of words, using `String.split`:
+	- `'Hi there, my name is Mike' => ['Hi', 'there', 'my', 'name', 'is', 'Mike']`
+
+2. Get `World` from the string `'Hello World!'` using `String.substr`:
+	- `'Hello World!' => 'World'`
+
+3. Capitalize a word using a combination of `String.toUpperCase` and `String.substr`:
+	- `roberto => 'Roberto'`
+
+4. Convert a string to a number using `parseInt`
+	- `'12' => 12`
+
+5. Convert a decimal number to a string with 2 decimal places using `Number.toFixed`
+	- `15.827993 => '15.82'`
+
+4. Join two arrays of strings using `Array.concat`.
+	- `['Mike', 'Emily'] ['Will', 'Ajay'] => ['Mike', 'Emily', 'Will', 'Ajay']`
+
+5. Take an array of words and turn them into a string with pipes `|` between each word using `Array.join`.
+	- `['cloud', 'house', 'mountain', 'field'] => "cloud|house|mountain|field"`
+
+6. Make a new array of the 2nd and 3rd elements of an array using `Array.slice`
+	- `['hippo', 'giraffe', 'lion', 'hyena'] => ['giraffe', 'lion']`
+
+7. Find the index of the word "pizza" in the following array with  `Array.indexOf`.
+	- `['salad', 'burger', 'pizza', 'soup'] => 2`
+
+8. Sum the contents on an array of numbers using `Array.reduce`.
+	- `[1,2,3] => 6`
+
+9. Take an array of strings and turn it into an array of numbers indicating the length of each string using `Array.map`.
+	- `['Los Angeles', 'New York', 'Huston'] => [11, 8, 6]`
+
+10. Take an array of numbers and return only the ones that are divisible by 3. You will need `Array.filter` and the modulus `%` operator.
+	- `[1,2,3,4,5,6,7,8,9,10] => [3,6,9]`
+
+11. Order an array of strings alphabetically with `Array.sort`.
+	- `['Mohammed', 'Katheryn', 'Aaron', 'Amanda'] => ['Aaron', 'Amanda', 'Katheryn', 'Mohammed']`
+
+12. Turn an array of numbers into an array of strings using `Array.map` and `Number.toString`
+	- `[5,10,15,20,25] => ['5','10','15','20','25']`
+
+13. Convert a phrase to title case, using `String.split`, `Array.map`, `String.toUpperCase`, `String.substr` and `Array.join`
+	- `'the wind in the willows' => 'The Wind In The Willows'`
