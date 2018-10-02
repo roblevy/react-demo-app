02e51b14c60cdb9ad24165c34f76e331aa82ef82
Ellie Farrer <elliefarrermusic@gmail.com>
2018-10-02 08:58:53 +0100

    Added functions lab

---
+++ week2/3-functions-lab/my-code/js/app.js
@@ -0,0 +1,47 @@
+// Question 1
+function maxOfTwoNumbers() {
+
+}
+
+// Question 2
+function maxOfThree() {
+
+}
+
+// Question 3
+function isCharacterAVowel() {
+
+}
+
+// Question 4
+function sumArray() {
+
+}
+
+
+// Question 4
+function multiplyArray() {
+
+}
+
+
+// Question 5
+function numberOfArguments(){
+
+}
+
+
+// Question 6
+function reverseString(){
+
+}
+
+
+// Question 7
+function findLongestWord() {
+
+}
+
+
+// Question 8
+function filterLongWords() {
