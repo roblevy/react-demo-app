549db99989bbfb65bab63aabf61a5b795007eebb
Rob Levy <robert.g.levy@gmail.com>
2018-10-02 12:17:57 +0100

    Updated functions lab

--- week2/4-intro-to-functions/2-writing-functions-lab/my-code/independent-practice.js
+++ week2/4-intro-to-functions/2-writing-functions-lab/my-code/independent-practice.js
@@ -1,17 +1,38 @@
 /* eslint-disable no-unused-vars */
 
-//  1. Write a function `lengths` that accepts a single parameter as an argument, namely
+// 1. Write a Javascript function called `addThenSquare`
+// This function should accept two arguments, which you can assume will be numbers.
+//
+// Your function should add the two numbers then square the result.
+// Test your function in Chrome with various numbers.
+function addThenSquare(firstNumber, secondNumber) {
+  // Your code here
+}
+
+// 2. Write a function which accepts a single argument.
+//    The function assumes the argument is an array and tries to return the third element.
+//    If the array is shorter than three elements, the function returns early with an
+//    appropriate message.
+function thirdElement(inputArray) {
+  // Your code goes here
+}
+const firstResult = thirdElement(['a', 1, -1000]);
+const secondResult = thirdElement([100, 'abc']);
+console.log(firstResult);
+console.log(secondResult);
+
+// 3. Write a function `getLengths` that accepts a single argument as an argument, namely
 // an array of strings. The function should return an array of numbers where each
 // number is the length of the corresponding string.
+// e.g. const someStrings = ['abc', 'hello', 'bananas'];
+//      const someLengths = getLengths(someStrings);
+//      console.log(someLengths);
+// This should show [3, 5, 7] in the console.
+function getLengths(inputArray) {
+  const lengths = [];
+  // Now use a loop and Array.push...
+}
 
-
-// 2. Write a Javascript function called `transmogrifier`
-// This function should accept three arguments, which you can assume will be numbers.
-// Your function should return the "transmogrified" result
-//
-// The transmogrified result of three numbers is the product of the first two numbers,
-// raised to the power of the third number.
-
-
-// 3.  Write a function `wordReverse` that accepts a single argument, a string. The method should return a string with the order of the words reversed. Don't worry
+// 4.  Write a function `wordReverse` that accepts a single argument, a string.
+// The method should return a string with the order of the words reversed. Don't worry
 // about punctuation.
