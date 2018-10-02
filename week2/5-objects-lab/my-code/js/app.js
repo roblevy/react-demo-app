59f7e51d97b7b4c9869f39055050d77d1b025f72
Ellie Farrer <elliefarrermusic@gmail.com>
2018-10-02 14:41:51 +0100

    Added objects lab mycode

---
+++ week2/5-objects-lab/my-code/js/app.js
@@ -0,0 +1,22 @@
+// Remember to comment out your work after each solution so the code doesn't interfere with your new one. Make sure you can see 'JS Loaded' in the console before you begin!
+console.log('JS Loaded');
+
+
+// 1 - Cars
+// * your code here *
+
+
+// 2 - Ice Cream
+// * your code here *
+
+
+// 3 - Countries
+// * your code here *
+
+
+// 4 - Pets
+// * your code here *
+
+
+// 5 - The Bag
+// * your code here *
