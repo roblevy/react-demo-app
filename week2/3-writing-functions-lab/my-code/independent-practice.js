fd00522d7e5018b1ff6471f80be8757566145b4a
Ellie Farrer <elliefarrermusic@gmail.com>
2018-10-02 09:35:40 +0100

    Added function writing lab

---
+++ week2/3-writing-functions-lab/my-code/independent-practice.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+
+//  1. Write a function `lengths` that accepts a single parameter as an argument, namely
+// an array of strings. The function should return an array of numbers where each
+// number is the length of the corresponding string.
+
+
+// 2. Write a Javascript function called `transmogrifier`
+// This function should accept three arguments, which you can assume will be numbers.
+// Your function should return the "transmogrified" result
+//
+// The transmogrified result of three numbers is the product of the first two numbers,
+// raised to the power of the third number.
+
+
+// 3.  Write a function `wordReverse` that accepts a single argument, a string. The method should return a string with the order of the words reversed. Don't worry
+// about punctuation.
