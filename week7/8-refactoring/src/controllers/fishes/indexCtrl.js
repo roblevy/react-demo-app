ae044d3575c8ae88e46d8999a3af9b784ad86798
Rob Levy <robert.g.levy@gmail.com>
2018-11-07 11:17:13 +0000

    Added filtering

--- week7/8-refactoring/src/controllers/fishes/indexCtrl.js
+++ week7/8-refactoring/src/controllers/fishes/indexCtrl.js
@@ -3,8 +3,18 @@ function indexCtrl($scope, $http) {
     method: 'GET',
     url: '/api/fishes'
   }).then(result => {
-    $scope.fishes = result.data;
+    // allFishes is every fish in the database
+    $scope.allFishes = result.data;
+    // filteredFishes will contain the filtered fish once
+    // the user has entered a search term
+    $scope.filteredFishes = $scope.allFishes;
   });
+  $scope.handleFilterSubmit = function() {
+    console.log('Filter form submitted!', $scope.searchTerm);
+    $scope.filteredFishes = $scope.allFishes.filter(fish =>
+      fish.name.toLowerCase().startsWith($scope.searchTerm.toLowerCase())
+    );
+  };
 }
 
 export default indexCtrl;
