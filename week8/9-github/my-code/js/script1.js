918c6ed3a08ca9a75cf4ca8f8cb693651ac196e7
Rob Levy <robert.g.levy@gmail.com>
2018-11-16 09:53:48 +0000

    Github merge conflicts

---
+++ week8/9-github/my-code/js/script1.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log('The js file is linked');
---
+++ week8/9-github/my-code/js/script2.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+function combineStrings(str1, str2) {
+  return `${str1} -- ${str2}`;
+}
+
+function mergeStrings(str1, str2) {
+  return `${str1}${str2}`;
+}
+
+function concatStrings(str1, str2) {
+  return `${str1} +=+ ${str2}`;
+}
+
+// Rob's function here
+function quadruplify(x) {
+  return x ** 4;
+}
+
+function retrunStr1(str1) {
+  return str1;
+}
+
+// Ellie's function here
+function mergeAndUppercase(str1, str2) {
+  return `${str1.toUpperCase()} ${str2.toUpperCase()}`;
+}
+
+function foo(a) {
+  return a;
+}
+
+function bar(a, b) {
+  return a + b;
+}
+
+console.log('The second js file is linked');
