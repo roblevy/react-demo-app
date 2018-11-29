3f32ef05c99b47de2df55ea3162a732eeba1b21f
matthunterking <matthew85king@gmail.com>
2018-11-29 15:22:59 +0000

    Added solution code

---
+++ week10/13-auth-lab/solution-code/my-code/src/components/burgers/ImageColumn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ImageColumn({ burger }) {
+  return (
+    <div className="column is-6">
+      <img src={burger.image} alt={burger.name}/>
+    </div>
+  );
+}
+
+export default ImageColumn;
