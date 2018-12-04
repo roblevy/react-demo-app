add6bb74f8cf8fa947c867bc4982474a3e9738d0
Rob Levy <robert.g.levy@gmail.com>
2018-12-04 20:16:13 +0000

    Front-end testing complete

--- week11/5-front-end-testing/test/client/template.js
+++ week11/5-front-end-testing/test/client/template.js
@@ -1,13 +1,26 @@
-/* global describe, it */
+/* global describe,it,beforeEach */
+// See the notes in `helper.js` for lots of additional explanation.
+// Run `yarn test:client` to run all the tests.
 import React from 'react';
-// See the notes in `helper.js` to explain what all this is.
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 // import your component to be tested here.
 // e.g. import BurgersIndex from '../../../src/components/burgers/Index';
 
+// You might need to add some test data
+// e.g. const testData = [...];
+
 describe('component name goes here', () => {
+  // You could have a beforeEach which sets up the component.
+  // You'll have to save the component to a global once you've created it
+  // to make it available to all the test cases. See Messages/Sidebar_spec.js
+  // for an example of this.
+  beforeEach(done => {
+    // Do some stuff then...
+    done();
+  });
+
   // A test case should describe one real-world job
   // of a component
   it('should... (test case goes here)', done => {
@@ -21,7 +34,7 @@ describe('component name goes here', () => {
     // Enzyme allows querySelector style selection of DOM elements
     // via component.find.
     // e.g. expect(component.find('div').length).to.eq(3);
-    
+
     // Then call done when all the assertions are finished.
     done();
   });
